82bb523ceac92ed4603df0b252c88555be978b32
Hamza <hamzashah.dev@gmail.com>
2024-03-27 18:10:39 +0500

    added video in modal

--- backend/modals/ProjectModal.js
+++ backend/modals/ProjectModal.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ProjectsSchema = mongoose.Schema({
-    title: String,
-    desc: String,
-    link: String,
-    github: String,
-    img: String,
-    featured: Boolean,
-    remove: Boolean
-})
+  title: String,
+  desc: String,
+  link: String,
+  github: String,
+  img: String,
+  video: String,
+  featured: Boolean,
+  remove: Boolean,
+});
 
-const ProjectModal = mongoose.model('projects', ProjectsSchema)
+const ProjectModal = mongoose.model("projects", ProjectsSchema);
 module.exports = ProjectModal;
